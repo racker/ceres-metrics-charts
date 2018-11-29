ebfb66713a8a3cf97dd4c55116745696d5ef1f48
Mritunjay Kumar <mritunjay.kumar@rackspace.com>
2018-11-28 23:08:21 -0800

    AWS to GCP migration of Helm charts

--- create.sh
+++ create.sh
@@ -3,6 +3,8 @@
 helm install --name data --namespace metrics ./influxdb/
 helm install --name polling --namespace metrics ./telegraf-s/
 helm install --name hosts --namespace metrics ./telegraf-ds/
+helm install --name lookup --namespace metrics ./redis/
+helm install --name routing --namespace metrics ./tenant-routing-service/
 helm install --name ingestion --namespace metrics ./ingestion-service/
 helm install --name query --namespace metrics ./query-service/
-helm install --name routing --namespace metrics ./tenant-routing-service/
+helm install --name rollup --namespace metrics ./kapacitor/
